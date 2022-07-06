816722c14cee0612f0e7b5ac24f2aa33e149f504
harrisondickens <harrison.dickens0122@gmail.com>
2022-07-06 15:31:30 +0100

    Retrieves Lanturn back_default image from the PokeApi and displays to user. Looking to incorporate Recoil into project.

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,26 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import axios from "axios";
+import {PokemonModel} from "./PokemonStore";
+
+const baseURL = "https://pokeapi.co/api/v2/pokemon/lanturn";
 
 function App() {
+  const [pokemon, setPokemon] = React.useState({sprites: {back_default: ""}});
+  React.useEffect(() => {
+    axios.get<PokemonModel>(baseURL).then((response) => {
+      setPokemon(response.data);
+    });
+  }, []);
+  if (!pokemon) return null;
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={pokemon.sprites.back_default} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
       </header>
     </div>
   );
---
+++ src/PokemonStore.tsx
@@ -0,0 +1,10 @@
+import React  from 'react';
+
+
+type SpritesModel = {
+    back_default: string
+}
+
+export type PokemonModel ={
+    sprites: SpritesModel;
+}
